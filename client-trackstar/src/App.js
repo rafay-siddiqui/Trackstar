41d704837e454babda618aa7ba913d28cd950481
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-07-12 15:29:01 -0400

    Display interactive map on homepage

--- client-trackstar/src/App.js
+++ client-trackstar/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav className="navbar">
+        <h1>Trackstar</h1>
+        <ul>
+          <li>Home</li>
+          <li>Tracks</li>
+          <li>History</li>
+        </ul>
+      </nav>
+      <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
     </div>
   );
 }
