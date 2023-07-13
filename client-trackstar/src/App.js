1a33d13fa634a6b7801577cad752ab09377eba22
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-07-13 15:30:01 -0400

    Implement location search that repositions map to specified address

--- client-trackstar/src/App.js
+++ client-trackstar/src/App.js
@@ -1,38 +1,63 @@
 import './App.css';
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, ZoomControl, useMapEvents, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, ZoomControl, useMap, useMapEvents, Marker } from 'react-leaflet'
 import { useState } from 'react';
 
 import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+import axios from 'axios'
+
 let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-    iconSize: [25, 41],
-    shadowSize: [41, 41],
-    iconAnchor: [12, 41], 
-    shadowAnchor: [14, 41],  
-    popupAnchor: [0, -41] 
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  shadowSize: [41, 41],
+  iconAnchor: [12, 41],
+  shadowAnchor: [14, 41],
+  popupAnchor: [0, -41]
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
 function App() {
-  const [markersPos, setMarkersPos] = useState([[0,0]])
+  const [markersPos, setMarkersPos] = useState([[0, 0]])
+  const [mapCenter, setMapCenter] = useState([43.5588, -79.7116])
+  const [searchLocation, setSearchLocation] = useState('')
+  const [creatingRoute, setCreatingRoute] = useState(false)
 
-  function MyComponent() {
+  function CreateMarker() {
     useMapEvents({
       click: (e) => {
         var lat = e.latlng.lat
         var lng = e.latlng.lng
-        setMarkersPos([...markersPos,[lat,lng]])
+        setMarkersPos([...markersPos, [lat, lng]])
       },
     })
+
     return null
   }
 
+  const handleSearchLocationChange = (e) => {
+    setSearchLocation(e.target.value);
+  }
+
+  const handleLocationSearch = async (e) => {
+    if (e.key === 'Enter') {
+      const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${e.target.value}`)
+      if (response.data[0]) {
+        const { lat, lon } = response.data[0];
+        setMapCenter([parseFloat(lat), parseFloat(lon)])
+      }
+    }
+  }
+
+  function CenterMap() {
+    const map = useMap();
+    map.flyTo(mapCenter, map.getZoom());
+    return null;
+  }
 
   return (
     <div className='App'>
@@ -46,22 +71,24 @@ function App() {
       </nav>
 
       <div className="routemaker-map">
-        <MapContainer center={[43.5588, -79.7116]} zoom={13} zoomControl={false}
+        <MapContainer center={mapCenter} zoom={13} zoomControl={false}
           style={{ height: "100vh", width: "100%" }} >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
           <ZoomControl position='bottomright' />
-          <MyComponent />
+          <CreateMarker />
           {markersPos.map((marker) => {
-            return <Marker key={[...marker]} position={[...marker]}/>
+            return <Marker key={[...marker]} position={[...marker]} />
           })}
+          <CenterMap />
         </MapContainer>
       </div>
 
       <div className="routemaker-form">
-        <input id='map-location' type='text' placeholder='Location'></input>
+        <input id='map-location' type='text' placeholder='Location' value={searchLocation} 
+        onChange={handleSearchLocationChange} onKeyUp={handleLocationSearch}></input>
         <span>Option 2</span>
       </div>
 
