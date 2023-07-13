ee2ed9ac549e853724ce6bab27cd14c053ff7a1b
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-07-13 10:22:42 -0400

    Make map take up entire screen and have navbar overlapping the map with transparency

--- client-trackstar/src/App.js
+++ client-trackstar/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='App'>
       <nav className="navbar">
-        <h1>Trackstar</h1>
+        <h1>TRACKSTAR</h1>
         <ul>
           <li className="navitem">Home</li>
           <li className="navitem">Tracks</li>
@@ -14,22 +14,15 @@ function App() {
         </ul>
       </nav>
 
-      <div className="routemaker">
-        <div className="routemaker-menu">
-          <span>Enter Location</span>
-          <span>Create Route</span>
-        </div>
         <div className="routemaker-map">
-          {/* <h2>MapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMap</h2> */}
-          <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: "75vh", width: "100%" }}>
+          <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: "92vh", width: "100%" }}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
           </MapContainer>
         </div>
-      </div>
-      
+
     </div>
   );
 }
