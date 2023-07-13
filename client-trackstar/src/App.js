039b3514bfc78673a2151d1a795634a2532b4c13
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-07-13 09:57:56 -0400

    setup layout of route-making menu and the map side-by-side

--- client-trackstar/src/App.js
+++ client-trackstar/src/App.js
@@ -8,17 +8,28 @@ function App() {
       <nav className="navbar">
         <h1>Trackstar</h1>
         <ul>
-          <li>Home</li>
-          <li>Tracks</li>
-          <li>History</li>
+          <li className="navitem">Home</li>
+          <li className="navitem">Tracks</li>
+          <li className="navitem">History</li>
         </ul>
       </nav>
-      <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: "100vh", width: "100%" }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
+
+      <div className="routemaker">
+        <div className="routemaker-menu">
+          <span>Enter Location</span>
+          <span>Create Route</span>
+        </div>
+        <div className="routemaker-map">
+          {/* <h2>MapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMapMap</h2> */}
+          <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: "75vh", width: "100%" }}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </MapContainer>
+        </div>
+      </div>
+      
     </div>
   );
 }
