591af92967040d36688d4fe0dd19fe2d5103eab4
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-07-22 13:30:58 -0400

    add .env.example file to server

--- server-trackstar/src/index.js
+++ server-trackstar/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`TRACKSTAR database server running on ${PORT}`);
