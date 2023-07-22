625443612c50d117a31643154efac2ff48ce9ce0
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-07-22 15:43:00 -0400

    set headers in server index.js

--- server-trackstar/src/index.js
+++ server-trackstar/src/index.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const user_model = require('./user_model')
 
+app.use(express.json())
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers', 'Access-Control-Allow-Origin');
+  next();
+})
+
 user_model.createDB();
 user_model.createTables();
 user_model.seedDatabase();
