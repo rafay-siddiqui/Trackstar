d63c1530e87872f2b6fb93b5f9335ea577ce0d3e
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-08-11 11:13:03 -0400

    change intensity ranges to provide amin and max MET instead of a single value

--- client-trackstar/src/CalorieCalculator.js
+++ client-trackstar/src/CalorieCalculator.js
@@ -26,28 +26,37 @@ function CalorieCalculator({ distance, unit, activity, routeName }) {
 
   useEffect(() => {
     function footMET(pace) {
-      if (unit === 'km') pace *= 0.621371
-      if (pace <= 2.0) return 2.8;
-      if (pace <= 2.5) return 3.0;
-      if (pace <= 3.0) return 3.3;
-      if (pace <= 3.5) return 3.8;
-      if (pace <= 4.0) return 5.0;
-      if (pace <= 4.5) return 7.0;
-      if (pace <= 5) return 8.3;
-      if (pace <= 6) return 9.8;
-      if (pace <= 7) return 11;
-      if (pace <= 8) return 11.8;
-      if (pace <= 10) return 14.5;
-      return 16 + (pace - 10);
+      let minMET, maxMET;
+      if (unit === 'km') pace *= 0.621371;
+
+      if (pace <= 2.0) { minMET = 2.5; maxMET = 2.8; }
+      else if (pace <= 2.5) { minMET = 2.8; maxMET = 3.0; }
+      else if (pace <= 3.0) { minMET = 3.0; maxMET = 3.3; }
+      else if (pace <= 3.5) { minMET = 3.3; maxMET = 3.8; }
+      else if (pace <= 4.0) { minMET = 3.8; maxMET = 5.0; }
+      else if (pace <= 4.5) { minMET = 5.0; maxMET = 7.0; }
+      else if (pace <= 5) { minMET = 7.0; maxMET = 8.3; }
+      else if (pace <= 6) { minMET = 8.3; maxMET = 9.8; }
+      else if (pace <= 7) { minMET = 9.8; maxMET = 11; }
+      else if (pace <= 8) { minMET = 11; maxMET = 11.8; }
+      else if (pace <= 10) { minMET = 11.8; maxMET = 14.5; }
+      else { minMET = 14.5; maxMET = 16 + (pace - 10); }
+
+      return { min: minMET, max: maxMET };
     }
+
     function bikeMET(pace) {
-      if (unit === 'km') pace *= 0.621371
-      if (pace < 10) return 4.0;
-      if (pace <= 12) return 6.8;
-      if (pace <= 14) return 8.0;
-      if (pace <= 16) return 10.0;
-      if (pace <= 20) return 12.0;
-      return 16.0;
+      let minMET, maxMET;
+      if (unit === 'km') pace *= 0.621371;
+
+      if (pace < 10) { minMET = 3.5; maxMET = 4.0; }
+      else if (pace <= 12) { minMET = 4.0; maxMET = 6.8; }
+      else if (pace <= 14) { minMET = 6.8; maxMET = 8.0; }
+      else if (pace <= 16) { minMET = 8.0; maxMET = 10.0; }
+      else if (pace <= 20) { minMET = 10.0; maxMET = 12.0; }
+      else { minMET = 12.0; maxMET = 16.0; }
+
+      return { min: minMET, max: maxMET };
     }
 
     if (activity === 'walking' || activity === 'running') {
@@ -58,13 +67,12 @@ function CalorieCalculator({ distance, unit, activity, routeName }) {
   }, [pace, activity, unit])
 
   useEffect(() => {
-    const getIntensity = () => {
-      let calcWeight = weight;
-      if (weightUnit === 'lbs') { calcWeight *= 0.453592 }
-      return (intensity * calcWeight * (parseInt(hours) + parseFloat(minutes / 60)))
-    }
-    setCaloriesBurned(getIntensity())
-  }, [intensity, weight, hours, minutes, weightUnit])
+    let calcWeight = weight;
+    if (weightUnit === 'lbs') { calcWeight *= 0.453592 }
+    const minCalories = intensity.min * calcWeight * (parseInt(hours) + parseFloat(minutes / 60));
+    const maxCalories = intensity.max * calcWeight * (parseInt(hours) + parseFloat(minutes / 60));
+    setCaloriesBurned({ min: minCalories, max: maxCalories });
+  }, [intensity, weight, hours, minutes, weightUnit]);
 
   const toggleWeightUnit = () => {
     if (weightUnit === 'kg') {
@@ -110,7 +118,10 @@ function CalorieCalculator({ distance, unit, activity, routeName }) {
         <button onClick={toggleWeightUnit}>{weightUnit}</button>
       </label>
       <br />
-      <span>{`Burned ${caloriesBurned.toFixed(0)} calories`}</span>
+      <span>{`Burned between ${parseFloat(caloriesBurned.min).toFixed(0)} and ${parseFloat(caloriesBurned.max).toFixed(0)} calories`}</span>
+      <br />
+      <span>{`intensity is ${intensity.min} and ${intensity.max} hours is ${hours} minutes is ${minutes} weight is ${weight} 
+      weight unit is ${weightUnit} pace is ${pace} distance is ${distance} activity is ${activity} unit is ${unit}`}</span>
 
       <br />
       <button onClick={saveWorkout}>Save workout</button>
@@ -130,7 +141,7 @@ function CalorieCalculator({ distance, unit, activity, routeName }) {
               </span>
               <br />
               <span>
-                {`Burned ${workoutHistory[workout].caloriesBurned.toFixed(0)} calories at ${workoutHistory[workout].weight} ${workoutHistory[workout].weightUnit}`}
+                {`Burned between ${parseFloat(workoutHistory[workout].caloriesBurned.min).toFixed(0)} and ${parseFloat(workoutHistory[workout].caloriesBurned.max).toFixed(0)} calories at ${workoutHistory[workout].weight} ${workoutHistory[workout].weightUnit}`}
               </span>
             </li>)
         })}
