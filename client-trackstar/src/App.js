a1bc135a117822af66f4a8015e47606367e0bf25
rafay-siddiqui <rafays.siddiqui@gmail.com>
2023-08-10 22:13:51 -0400

    add developer name to attributions in bottom-right corner

--- client-trackstar/src/App.js
+++ client-trackstar/src/App.js
@@ -511,7 +511,7 @@ function App() {
           style={{ height: "100vh", width: "100%" }} >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | Rafay Siddiqui'
           />
           <ZoomControl position='bottomright' />
           <CreateMarker />
